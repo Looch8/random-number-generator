c06a16ed0f645151d10ec8652e328f5484369d6e
Luke Wheldale <lukewheldale@gmail.com>
2023-11-11 17:59:53 +1030

    feat: decrease number button, condition to not let number go less than 0

--- src/components/ValueComponent.jsx
+++ src/components/ValueComponent.jsx
@@ -3,15 +3,21 @@ import { useState } from "react";
 function ValueComponent({ valueName }) {
 	const [value, setValue] = useState(0);
 
-	const handleValue = () => {
+	const increaseValue = () => {
 		return setValue(value + 1);
+	};
+
+	const decreaseValue = () => {
+		// Value can not be decreased less than 0
+		if (value > 0) return setValue(value - 1);
 	};
 
 	return (
 		<>
 			<h3>
 				{valueName} number: {value}
-				<button onClick={handleValue}>Increase value</button>
+				<button onClick={increaseValue}>Increase value</button>
+				<button onClick={decreaseValue}>Decrease value</button>
 			</h3>
 		</>
 	);
